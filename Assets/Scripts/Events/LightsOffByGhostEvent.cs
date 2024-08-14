a6b82a56e5ac482059591b0a86b8708ebd9ac21a
Aryan khera <aryan.khera@outscal.com>
2024-08-14 14:19:07 +0530

    Light off event

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -1,6 +1,18 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
-
+    [SerializeField] private int keysRequired;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out PlayerView player) &&
+            GameService.Instance.GetPlayerController().KeysEquipped == keysRequired)
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.SpookyGiggle);
+            GetComponent<Collider>().enabled = false;
+        };
+    }
 }
