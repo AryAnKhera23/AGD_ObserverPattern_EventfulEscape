854c6d47cee2d6bf354cdb919d4a49dbe4ea48e6
Aryan khera <aryan.khera@outscal.com>
2024-08-14 12:43:45 +0530

    Updated event service

--- Assets/Scripts/Interactables/KeyView.cs
+++ Assets/Scripts/Interactables/KeyView.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class KeyView : MonoBehaviour, IInteractable
 {
     [SerializeField] GameUIView gameUIView;
-    public void Interact()
+    
+    private void OnEnable() => EventService.Instance.OnKeyPickedUp.AddListener(onKeyPickedup);
+    private void OnDisable() => EventService.Instance.OnKeyPickedUp.RemoveListener(onKeyPickedup);
+    public void Interact() => EventService.Instance.OnKeyPickedUp.InvokeEvent();
+    
+
+    public void onKeyPickedup()
     {
+        int currentKeys = GameService.Instance.GetPlayerController().KeysEquipped;
         GameService.Instance.GetInstructionView().HideInstruction();
         GameService.Instance.GetSoundView().PlaySoundEffects(SoundType.KeyPickUp);
-        GameService.Instance.GetPlayerController().KeysEquipped++;
+        currentKeys++;
         gameUIView.UpdateKeyText();
 
         gameObject.SetActive(false);
