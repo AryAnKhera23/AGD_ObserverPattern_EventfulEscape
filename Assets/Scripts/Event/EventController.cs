176910843399930fe824c7cb135191f247541864
Aryan khera <aryan.khera@outscal.com>
2024-08-14 12:10:57 +0530

    Light Switch Final Commit

--- Assets/Scripts/Event/EventController.cs
+++ Assets/Scripts/Event/EventController.cs
@@ -5,9 +5,9 @@ public class EventController
 {
     public event Action baseEvent;
     
-    public void SubscribeToEvents(Action listener) => baseEvent += listener;
+    public void AddListener(Action listener) => baseEvent += listener;
 
-    public void UnsubscribeToEvents(Action listener) => baseEvent -= listener;
+    public void RemoveListener(Action listener) => baseEvent -= listener;
     
     public void InvokeEvent() => baseEvent?.Invoke();
 }
